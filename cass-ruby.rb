bb2dc3996b568262ec16586420fa53c08578f0bd
Cevaris <cevaris@gmail.com>
2014-02-25 18:25:29 -0700

    added opt parse

--- cass-ruby.rb
+++ cass-ruby.rb
@@ -2,6 +2,7 @@
 
 require 'cassandra-cql'
 require 'json'
+require 'optparse'
 
 
 def connect_db
@@ -25,8 +26,38 @@ end
 def execute
 	TweetStream::Client.new.sample do |status|
 	  payload = { events: status, app: { id: app.uuid } }.to_json    
+	  puts payload
 	end
 end
 
 
 
+
+
+if __FILE__ == $0
+
+  options = {}
+
+  optparse = OptionParser.new do|opts|
+    opts.banner = "Usage: list_tweets.rb [options]"
+
+    options[:verbose] = false
+      opts.on( '-v', '--verbose', 'Output more information' ) do
+      options[:verbose] = true
+    end
+
+    options[:event] = 'Boulder flash floods 2013'
+    opts.on( '-e', '--event EVENT_NAME', 'Name of event which to output keywords' ) do|value|
+      options[:event] = value
+    end
+
+    opts.on( '-h', '--help', 'Display this screen' ) do
+      puts opts
+      exit
+    end
+  end
+
+  optparse.parse!
+
+  execute()
+end
